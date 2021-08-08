a0f174acbf159ae7c386624212b2bc0530c4146a
Swarnima Srivastava <swarnimasrivastava2019@gmail.com>
2021-08-08 15:14:36 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import images from "./public/Weather icon.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Weather(){
+
+    const [a, b] = useState("");
+    const [c, f] = useState("");
+    const [y, z] = useState("");
+
+    function Changed(event){
+        f(event.target.value);
+    }
+
+function Clicked(e){
+    e.preventDefault()
+    console.log("clicked");
+    const d = {c};
+    
+    axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + c + "&appid={API key}").then((resp) => {
+        console.log(resp.data.main.temp);
+        z(Math.round(resp.data.main.temp-273) + "°C");
+     }).catch((error) => {
+         console.log(error);
+     })
+   
+  
+    const config = {
+        method: "POST" ,
+        url: "https://reqres.in/api/users" ,
+        headers: {
+            "Content-type": "application/json"
+        },
+        data: JSON.stringify({temp: d})
+    }
+    axios(config).then((res) => {
+        console.log(res.data);
+        b("The Temperature in " + res.data.temp.c + " is");
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 }
 
-export default App;
+return(<div id = "uy">
+ <h1 className = "styl" >   <img src =  {images} alt = "Hello" className = "imager" />   WEATHER API </h1> 
+    <form action = "" method = "post" >
+<input type = "text" placeholder = "Enter City Name" id = "lolo" onChange = {Changed} ></input>
+<button type = "submit" onClick = {Clicked}  >Click</button> 
+<h1> {a}  {y}  </h1>
+</form> 
+</div>);
+}
+
+export default Weather;
